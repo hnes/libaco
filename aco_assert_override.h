8e433521b9fdbcc7b5d935fb46b89398ec0cb168
Sen Han <00hnes@gmail.com>
2018-07-07 08:06:59 +0800

    fix some noncompliant include guards (issue https://github.com/hnes/libaco/issues/1, reported by Markus Elfring @elfring)

--- aco_assert_override.h
+++ aco_assert_override.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef _ACO_ASSERT_OVERRIDE_H
-#define _ACO_ASSERT_OVERRIDE_H
+#ifndef ACO_ASSERT_OVERRIDE_H
+#define ACO_ASSERT_OVERRIDE_H
 
 #include "aco.h"
 
